80ee4eb8ac57db8c4dc20cf07f7a0fc9eabd2963
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-07-12 18:30:28 +0100

    project setup

---
+++ src/BmxStreetsMapManager.Core/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace BmxStreetsMapManager.Core;
+
+public static class Constants
+{
+    public static readonly string WorkingDirectory = Path.Combine(BmxStreetsConsts.BmxStreetsDirectory, "Bmx Streets Map Manager");
+}
+
+public static class BmxStreetsConsts
+{
+    public static readonly string BmxStreetsDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "BMX Streets");
+    public static readonly string BmxStreetsMapDirectory = Path.Combine(BmxStreetsDirectory, "Maps");
+}
