852a6af6fb3eb321c764b94d8185e795c1a007bc
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-07-20 12:24:30 +0100

    add migrations

---
+++ src/BmxStreetsMapManager.Core/Migrations/20240720112127_InitialCreate.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BmxStreetsMapManager.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Maps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LocalPath = table.Column<string>(type: "TEXT", nullable: false),
+                    LocalName = table.Column<string>(type: "TEXT", nullable: false),
+                    ModIOId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ModIOName = table.Column<string>(type: "TEXT", nullable: true),
+                    ModIOVersion = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserConfigs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ModIOApiToken = table.Column<string>(type: "TEXT", nullable: true),
+                    ModIOUserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserConfigs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MapProfiles",
+                columns: table => new
+                {
+                    ProfileId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MapId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapProfiles", x => new { x.ProfileId, x.MapId });
+                    table.ForeignKey(
+                        name: "FK_MapProfiles_Maps_MapId",
+                        column: x => x.MapId,
+                        principalTable: "Maps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MapProfiles_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapProfiles_MapId",
+                table: "MapProfiles",
+                column: "MapId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MapProfiles");
+
+            migrationBuilder.DropTable(
+                name: "UserConfigs");
+
+            migrationBuilder.DropTable(
+                name: "Maps");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}
