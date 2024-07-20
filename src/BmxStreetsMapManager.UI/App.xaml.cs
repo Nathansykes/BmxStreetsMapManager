6356daa332035cfeb3ddafe8300084084e564b11
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-07-20 12:28:55 +0100

    add migrate to startup

--- src/BmxStreetsMapManager.UI/App.xaml.cs
+++ src/BmxStreetsMapManager.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using BmxStreetsMapManager.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Windows;
 
 namespace BmxStreetsMapManager.UI;
 /// <summary>
@@ -9,5 +11,13 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        Task.Run(MigrateDatabase);
+    }
+
+    private static async Task MigrateDatabase()
+    {
+        using var context = new ApplicationDbContext();
+        await context.Database.EnsureCreatedAsync();
+        await context.Database.MigrateAsync();
     }
 }
